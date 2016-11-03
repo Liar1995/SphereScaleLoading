4591ee0761f3e8bcc3db0027e51bfd9a1e8ef10e
summer <sunmeng1995@outlook.com>
2016-11-03 10:20:57 +0800

    add simpel gif image~

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # SphereScaleLoading
+![](blob:chrome-extension://cdneggiaafcmelgcaiihmhiagieomgfj/db597f79-576c-483e-8a64-7312d453bfc6)
--- app/src/main/java/com/spherescaleloading/MainActivity.java
+++ app/src/main/java/com/spherescaleloading/MainActivity.java
@@ -1,13 +1,18 @@
 package com.spherescaleloading;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+//    circleRadius：设置圆的半径
+//    circleSpacing：设置圆间距
+//    cycle: 设置动画周期，即某一个圆从最大状态到最小状态，最后又恢复到最大状态的时间，单位是毫秒
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GyqVideoAnim view= (GyqVideoAnim) findViewById(R.id.view);
     }
 }
